f3ac3848f67d6da1543c299b563be443cc3c4fe2
Svoyclub <32313728+Svoyclub@users.noreply.github.com>
2018-07-22 03:09:18 +0300

    Create AbonementComponent.php

---
+++ AbonementComponent.php
@@ -0,0 +1,25 @@
+<?php
+
+//	AbonementComponent
+
+namespace frontend\components;
+
+use yii\base\Component;
+use yii\helpers\Html;
+
+class AbonementComponent extends Component{ // объявляем класс
+	public $content;
+	
+	public function init(){ //	функция инициализации. Если данные не будут переданы в компонент, то он выведет текст "Текст по умолчанию"
+		parent::init();
+		$this->content= 'Текст по умолчанию';
+	}
+
+       public function display($content=null){ //	функция отображения данных
+		if($content!=null){ //	проверка строки на пустоту
+			$this->content= $content;
+		}
+		echo Html::encode($this->content); //	вывод данных
+	}
+	
+}
